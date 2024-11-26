aaa9350974f9d9cdf3a48cac26e04a0676f6c903
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-11-26 21:44:10 +0530

    26/11/24

--- src/components/qrscanner.jsx
+++ src/components/qrscanner.jsx
@@ -15,7 +15,7 @@ const QRScanner = () => {
     setIsLoading(true);
     try {
       // Use relative URL with a leading slash
-      const response = await fetch('/qr_code/get_qr', {
+      const response = await fetch('http://20.84.153.108:8000/qr_code/get_qr', {
         method: 'GET',
         headers: {
           // Optional: set headers if needed
@@ -49,7 +49,7 @@ const QRScanner = () => {
     setIsLoading(true);
     try {
       // Use relative URL with a leading slash
-      const response = await fetch('/tracking/start', {
+      const response = await fetch('http://20.84.153.108:8000/tracking/start', {
         method: 'POST',
         headers: {
           // Optional: set headers if needed
